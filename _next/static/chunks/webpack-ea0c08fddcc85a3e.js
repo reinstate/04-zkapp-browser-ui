070cd3aedb6cfc19a6b0c1ba84d8d3295851073d
rezaghalbi <rezaghalbi123@gmail.com>
2022-12-07 01:13:13 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-ea0c08fddcc85a3e.js
+++ _next/static/chunks/webpack-ea0c08fddcc85a3e.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "c0dc3e719a732daf" + ".css";
+/******/ 			return "static/css/" + "c1c543440aa655e7" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
